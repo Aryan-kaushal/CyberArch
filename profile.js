d0f5aeff80cb2e4065176462b2a91d04eb633778
Aryan <157451693+Aryan-kaushal@users.noreply.github.com>
2024-10-07 13:33:55 +0530

    Add files via upload

--- profile.js
+++ profile.js
@@ -1,3 +1,8 @@
 function viewResume() {
     alert("Resume is not available right now.");
 }
+
+function toggleNav() {
+    const nav = document.getElementById('navbar');
+    nav.classList.toggle('active');
+}
